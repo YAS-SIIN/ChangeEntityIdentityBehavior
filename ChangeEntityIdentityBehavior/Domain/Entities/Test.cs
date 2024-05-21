4dc3fc65130d3e6746266531de09bf4fdf341427
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-05-21 15:41:50 +0330

    Add project files.

---
+++ ChangeEntityIdentityBehavior/Domain/Entities/Test.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChangeEntityIdentityBehavior.Domain.Entities;
+
+public class Test
+{
+  public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ ChangeEntityIdentityBehavior/Infrastructure/Data/MainContext.cs
@@ -0,0 +1,34 @@
+﻿using ChangeEntityIdentityBehavior.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace ChangeEntityIdentityBehavior.Infrastructure.Data;
+
+public class MainContext : DbContext 
+{
+
+    public MainContext() : base()
+    {
+
+    }
+    #region Test    
+    public DbSet<Test> Tests { get; set; }
+    #endregion
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //var mutableProperties = modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.Name == "Status"));
+
+        #region Employee 
+
+        #endregion
+        var testBuilder = modelBuilder.Entity<Test>();
+        testBuilder.ToTable("Employee", schema: "EMP");
+        testBuilder.Property(c => c.Id).ValueGeneratedOnAdd();
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MainContext).Assembly); base.OnModelCreating(modelBuilder);
+    }
+
+}
